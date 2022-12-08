5f4be14a62db4a124e18892a4634eafe9dbbc98b
KrzysztofKinert <kinert.krzysztof@gmail.com>
2022-12-08 16:00:34 +0100

    Add project files.

---
+++ ZrzutEkranu/MainWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZrzutEkranu
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            SelectArea area = new SelectArea();
+            this.Hide();
+            area.Show();
+        }
+    }
+}
